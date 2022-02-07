431cb66b62ad35785b007065b7642c1157d4cdb6
shani kupiec <shani.kupiec@gmail.com>
2022-02-07 18:03:57 +0200

    update three

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,13 +1,14 @@
 "use strict";
 
 $(document).ready(init);
+const gOwnerEmail = 'shani.kupiec@gmail.com'
 
 function init() {
   console.log("Starting up");
-  renderProt();
+  renderProjs();
 }
 
-function renderProt() {
+function renderProjs() {
   var projs = getProjs();
   var strHTML = projs.map(
     (proj) =>
@@ -18,7 +19,7 @@ function renderProt() {
             <i class="fa fa-plus fa-3x"></i>
             </div>
         </div>
-        <img class="img-fluid" src="${proj.url}.jpg" alt="">
+        <img class="img-fluid" src="${proj.img}.jpg" alt="">
         </a>
         <div class="portfolio-caption">
         <h4>${proj.name}</h4>
@@ -34,9 +35,9 @@ function renderModal(projId) {
   var proj = getProjsById(projId);
   console.log('projId',projId, 'proj', proj);
   var strHTML = 
-      `<h2>${proj.name}</h2>
+      `<h2>'${proj.name}'</h2>
         <p class="item-intro text-muted">${proj.desc}</p>
-        <img class="img-fluid d-block mx-auto" src="${proj.url}.jpg" alt="">
+        <img class="img-fluid d-block mx-auto" src="${proj.img}.jpg" alt="">
         <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis
         dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate,
         maiores repudiandae, nostrum, reiciendis facere nemo!</p>
@@ -45,9 +46,27 @@ function renderModal(projId) {
             <li>Client: ${proj.name}</li>
             <li>Category: ${proj.title}</li>
         </ul>
+        <a class="m-5 text-primary link" href="${proj.url}" target="_blank"> Check It Out</a> </br>
         <button class="btn btn-primary" data-dismiss="modal" type="button">
         <i class="fa fa-times"></i>
         Close Project</button>`;
 
   $(".modal-body").html(strHTML);
+}
+
+function sendEmail(){
+    var email = $('[name=email]').val()
+    var subject = $('[name=sub]').val()
+    var message = $('[name=msg]').val()
+
+    $('[name=email]').val('')
+    $('[name=sub]').val('')
+    $('[name=msg]').val('')
+
+    if(!email || !subject || !message || !email.match("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")) return
+    
+    console.log('email',email, 'subject',subject, 'message', message);
+
+    var url = `https://mail.google.com/mail/?view=cm&fs=1&to=${gOwnerEmail}&su=${subject}&body=${message}`
+    window.open(url);
 }
--- js/services/gallery-service.js
+++ js/services/gallery-service.js
@@ -5,20 +5,24 @@ createProjs()
 
 function createProjs(){
     gProjs = [
-        createProj('Minesweeper', 2022),
-        createProj('Chess', 2022),
-        createProj('Todos', 2022)
+        createProj('101', 'Minesweeper', 'thinking game', 'try not to die','https://shanikupiec.github.io/Mine-Sweeper/', 'Jan - 2022'),
+        createProj('102', 'Chess', 'thinking game', 'helps beginners to play chess by showing optional moves','https://shanikupiec.github.io/ChessProj/', 'Jan - 2022'),
+        createProj('103', 'Ball', 'fun game', 'clear all the balls to win','https://shanikupiec.github.io/BallBoard/', 'Jan - 2022'),
+        createProj('104', 'Pacman', 'fun game', 'eat all food and stay away from ghost','https://shanikupiec.github.io/Pacman/', 'Jan - 2022'),
+        createProj('105', 'ToDo', 'helpful website', 'organize all your tasks in one place','https://shanikupiec.github.io/ToDos/', 'Jan - 2022'),
+        createProj('106', 'BookShop', 'helpful website', 'access ordering books online','https://shanikupiec.github.io/BookShop/', 'Jan - 2022')
     ];
 }
 
-function createProj(name, year) {
+function createProj(id, name, title, desc, url, date) {
   return {
-    id: name,
+    id,
     name,
-    title: name + "Best Project Ever",
-    desc: "lorem ipsum lorem ipsum lorem ipsum",
-    url: "img/portfolio/01-full",
-    publishedAt: year,
+    title,
+    desc,
+    img: "img/portfolio2/01-full",
+    url,
+    publishedAt: date,
     labels: ["Matrixes", "keyboard events"],
   };
 }
